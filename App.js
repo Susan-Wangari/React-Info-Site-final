438d11f5b54443b6f8ee4fc928b9f1a3601f8c73
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2022-02-10 23:55:13 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React  from "react";
+import  ReactDOM  from "react";
+import Navbar from "./Components/Navbar";
+import Main from "./Components/Main";
+
+export default function App () {
+    return (
+        <div className="container">
+        <Navbar />
+        <Main />
+        </div>
+    )
+};
+
